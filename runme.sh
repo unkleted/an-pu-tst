b36762c0fbd708d8f8e7b5431a9dab114e371d3f
Justin Davis <justn.davis@gmail.com>
2022-09-03 16:39:35 -0400

    silent

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $(curl -s -o /dev/null -w "%{http_code}" http://169.254.169.254/latest/meta-data/public-hostname) -eq 200 ]]; then 
-    export subset=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+    export subset=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
 else 
-    export subset=$(curl http://169.254.169.254/latest/meta-data/local-hostname) 
+    export subset=$(curl -s http://169.254.169.254/latest/meta-data/local-hostname) 
 fi
